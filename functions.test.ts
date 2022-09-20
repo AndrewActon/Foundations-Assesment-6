5c24a69d9d0aa7a14c65be167ae5918094111e74
Andrew Acton <andrew.acton.aa@gmail.com>
2022-09-19 19:21:31 -0600

    Unit tests

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,14 @@
 const {shuffleArray} = require('./utils')
+const {bots} = require('./data')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    
+    test('Be the same length', () => {
+        expect(shuffleArray.length).toEqual(bots.length)
+      })
+
+    test('Has all the same items', () => {
+        expect(shuffleArray).toEqual(expect.arrayContaining(bots))
+    })
+
 })
